0ab48ed78e67fbe89815d5023eed9d9ea1d82080
Javier Muriilo <jajomuri@espol.edu.ec>
2024-12-03 10:01:42 -0500

    mvn report

--- src/test/java/tests/LogOutTest.java
+++ src/test/java/tests/LogOutTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.testng.Assert;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -8,6 +10,8 @@ import org.testng.annotations.Test;
 public class LogOutTest extends BaseTest {
     @Test
     @Parameters({"user", "password"})
+    @Description("Test for logOut functionality")
+    @Step("Paso uno")
     public void logOutTest(@Optional("standard_user") String user, @Optional("secret_sauce")String password){
         loginPage.login(user, password);
         productPage.clickOnBurqerMenu();
